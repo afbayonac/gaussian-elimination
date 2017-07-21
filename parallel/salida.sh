63208db8623591543f48a3d26eceaddf9cbaab39
afbayonac <afbayonac@gmail.com>
2017-07-20 19:58:08 -0500

    :pencil: Edit bash

--- parallel/salida.sh
+++ parallel/salida.sh
@@ -5,10 +5,10 @@
 
 iteraciones=`expr 2`
 
-for j in 2 4 6 8 14 18 24
+for j in  4 #6 8 14 18 24
   do
   export OMP_NUM_THREADS=$j
-  for (( n = 100; n <= 30000; n = n + 100 ))
+  for (( n = 100; n <= 3000; n = n + 100 ))
     do
       sum=`expr 0`
       echo n=$n hilos=$j &>> salida_$j
