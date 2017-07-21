e04c8770b7aa98b34c7f5741c9a0ec0f6584c9ec
afbayonac <afbayonac@gmail.com>
2017-07-20 19:35:08 -0500

    :pencil: Edit bash

--- parallel/salida.sh
+++ parallel/salida.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+#SBATCH --job-name=OpenMPtest
+#SBATCH --output=openmp.out
+#SBATCH --error=openmp.err
+#SBATCH --time=01:00:00
+#SBATCH --partition=manycores24
+#SBATCH --men=34000
 
 iteraciones=`expr 2`
 
-for j in 2 4 6 8 14 18 24
+for j in 2 4 #
+6 8 14 18 24
   do
   export OMP_NUM_THREADS=$j
-  for (( n = 100; n <= 3000; n = n + 100 ))
+  for (( n = 100; n <= 30000; n = n + 100 ))
     do
       sum=`expr 0`
       echo n=$n hilos=$j &>> salida_$j
--- serial/salida.sh
+++ serial/salida.sh
@@ -5,7 +5,7 @@ iteraciones=`expr 2`
 for (( n = 100; n <= 3000; n = n + 100 ))
   do
     sum=`expr 0`
-    echo n=$n hilos=&>> salida
+    echo n=$n &>> salida
     for (( i = 0; i <= iteraciones; i++ ))
       do
         echo $(./run $n) >> salida
